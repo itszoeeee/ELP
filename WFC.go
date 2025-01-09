4fa8d247cb62d2227b21ab57a69c11a89c7d6b70
Mathis <ducher.mathis@gmail.com>
2025-01-09 13:10:23 +0100

    WFC : Choix aléatoire de la première tuile

--- WFC.go
+++ WFC.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 	"image"
 	"image/png"
+	"math/rand"
 	"os"
 )
+
+const DIM = 2
+
+const BLANK = 0
+const UP = 1
+const RIGHT = 2
+const DOWN = 3
+const LEFT = 4
+
+type gridItem struct {
+	collapsed bool  // Champ pour "collapsed"
+	options   []int // Un tableau de tableaux d'entiers
+}
 
 // Fonction pour ouvrir une image et la décoder
 func loadImage(path string) (image.Image, error) {
@@ -21,6 +35,35 @@ func loadImage(path string) (image.Image, error) {
 	}
 
 	return img, nil
+}
+
+func createTile() (Tiles [5]image.Image, err error) {
+	// Charger l'image pattern.png
+	for i := 0; i < 5; i++ {
+		Tiles[i] = image.Transparent
+	}
+
+	Tiles[0], err = loadImage("pattern/blank.png")
+	if err != nil {
+		return Tiles, fmt.Errorf("Erreur lors du chargement de l'image 'blank.png': %w", err)
+	}
+	Tiles[1], err = loadImage("pattern/up.png")
+	if err != nil {
+		return Tiles, fmt.Errorf("Erreur lors du chargement de l'image 'up.png': %w", err)
+	}
+	Tiles[2], err = loadImage("pattern/right.png")
+	if err != nil {
+		return Tiles, fmt.Errorf("Erreur lors du chargement de l'image 'right.png': %w", err)
+	}
+	Tiles[3], err = loadImage("pattern/down.png")
+	if err != nil {
+		return Tiles, fmt.Errorf("Erreur lors du chargement de l'image 'down.png': %w", err)
+	}
+	Tiles[4], err = loadImage("pattern/left.png")
+	if err != nil {
+		return Tiles, fmt.Errorf("Erreur lors du chargement de l'image 'left.png': %w", err)
+	}
+	return Tiles, nil
 }
 
 // Fonction pour créer une image vide avec une taille spécifique (largeur, hauteur)
@@ -46,32 +89,98 @@ func placeImageInMatrix(dst *image.RGBA, src image.Image, gridX, gridY, cellSize
 }
 
 func main() {
-	// Charger l'image pattern.png
-	blank, err := loadImage("pattern/blank.png")
-	up, err := loadImage("pattern/up.png")
-	right, err := loadImage("pattern/right.png")
-	down, err := loadImage("pattern/down.png")
-	left, err := loadImage("pattern/left.png")
+
+	// ----- Initialisation -----
+
+	// Création des tuiles
+	Tiles, err := createTile()
 	if err != nil {
-		fmt.Println("Erreur lors du chargement des images:", err)
+		fmt.Println(err)
 		return
 	}
 
-	// Dimensions de la "grande image" (matrice 3x3)
-	gridWidth, gridHeight := 3, 3
-	cellSize := up.Bounds().Dx() // On suppose que la cellule fait la même taille que les images chargées
+	// Création de la grille
+	var grid []*gridItem
 
-	// Créer une image vide qui servira de "grande image"
+	// Initialisation des éléments de la grille
+	for i := 0; i < DIM*DIM; i++ {
+		// Crée une nouvelle instance de gridItem
+		cell := &gridItem{
+			collapsed: false,                // Initialisé à false
+			options:   []int{1, 2, 3, 4, 5}, // Options fixes
+		}
+		grid = append(grid, cell) // Ajouter l'élément à la grille
+	}
+
+	// Dimensions de l'image de sortie
+	gridWidth, gridHeight := DIM, DIM
+	cellSize := Tiles[0].Bounds().Dx() // On suppose que la cellule fait la même taille que les images chargées
+
+	// Créer de l'image de sortie
 	outputImage := createEmptyImage(cellSize*gridWidth, cellSize*gridHeight)
 
-	// Choisir l'emplacement où on veut placer l'image (par exemple, case (1,1))
-	// Case (0,0) est en haut à gauche, case (2,2) est en bas à droite.
-	// Par exemple, on place l'image dans la case (1,1)
-	placeImageInMatrix(outputImage, blank, 1, 1, cellSize)
-	placeImageInMatrix(outputImage, up, 1, 0, cellSize)
-	placeImageInMatrix(outputImage, right, 2, 1, cellSize)
-	placeImageInMatrix(outputImage, down, 1, 2, cellSize)
-	placeImageInMatrix(outputImage, left, 0, 1, cellSize)
+	// ----- Fin de l'initialisation -----
+
+	// ----- Boucle principale -----
+
+	// TEST
+	// grid[0].options = []int{BLANK, UP}
+	// grid[1].options = []int{DOWN, LEFT}
+
+	// // Trier la grille par la longueur de "options" (inutile)
+	// sort.Slice(grid, func(i, j int) bool {
+	// 	return len(grid[i].options) < len(grid[j].options)
+	// })
+
+	// Trouver la longueur minimale de "options" dans la grille
+	minLength := len(grid[0].options)
+	for _, v := range grid {
+		if len(v.options) < minLength {
+			minLength = len(v.options)
+		}
+	}
+
+	// Créer une nouvelle tranche pour les éléments ayant la longueur minimale
+	var smallestItems []*gridItem
+	for _, v := range grid {
+		if len(v.options) == minLength {
+			smallestItems = append(smallestItems, v)
+		}
+	}
+
+	// Choisir aléatoirement les éléments de plus petites longueurs
+	var randomItem *gridItem = smallestItems[rand.Intn(len(smallestItems))] // Sélectionner une clé aléatoire parmi celles disponibles
+	fmt.Printf("\nÉlément sélectionné : %d\n", randomItem.options)          // Afficher l'élément sélectionné
+	randomItem.collapsed = true                                             // collapsed l'élément
+	var pick int = rand.Intn(len(randomItem.options))                       // choisir un option disponible (aléatoirement)
+	randomItem.options = []int{pick}
+
+	// ----- Condition d'affichage (si la tuile est collapsed) -----
+	for i := 0; i < DIM; i++ {
+		for j := 0; j < DIM; j++ {
+			var cell = grid[i+j*DIM]
+			if cell.collapsed {
+				var index = cell.options[0]
+				placeImageInMatrix(outputImage, Tiles[index], i, j, cellSize)
+			}
+		}
+	}
+
+	// Afficher la nouvelle grille
+	fmt.Println("Nouvelle grille:")
+	for _, v := range grid {
+		fmt.Println(v.options)
+	}
+
+	// Création de la tuile suivante
+	// var nextTile []*gridItem
+	for i := 0; i < DIM; i++ {
+		for j := 0; j < DIM; j++ {
+			// var index = grid[i+j*DIM]
+		}
+	}
+
+	// ----- Exportation de l'image -----
 
 	// Exporter l'image résultante dans un fichier PNG
 	outFile, err := os.Create("output.png")
